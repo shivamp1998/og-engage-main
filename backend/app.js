a23e6267fa55b5d83356f31bb5e29c19225eaabd
shivamp1998 <shivampandey98@gmail.com>
2022-02-26 15:05:55 +0530

    [login]=changes

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const db = require('./db/conn');
-
-const Route=require('./routes/router')
-app.use(express.json());
-app.use("/api",Route);
-
 const PORT = process.env.PORT || 3000;
 const passport = require('passport');
 const crypto = require('crypto');
+const connection = require('./db/conn');
 require('dotenv').config();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req,res) => {
     res.send('Test App')
 })
+
 
 
 app.listen(PORT, ()=> {
